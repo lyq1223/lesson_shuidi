1a9ad5f0c997c7ce2c38d0071a6a3a68ef8d2d5a
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-04-13 22:30:20 +0800

    post请求参数的获取

---
+++ 10.js
@@ -0,0 +1,20 @@
+//引入express框架
+const express = require('express');
+const bodyParser = require('body-Parser');
+//创建网站服务器
+const app = express();
+
+//拦截所有请求
+// extended:false 方法内部使用querystring模块处理请求参数的格式
+// extended:true方法内部使用第三方模块sq处理请求参数的格式
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/add', (req, res) => {
+    //接收post请求参数
+    res.send(req.body)
+})
+
+
+
+//端口监听
+app.listen(1111);
