fac10bdbb9bc31faae3c4b05fc4b497c2a859775
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-01 23:09:09 +0800

    rounter

--- index.js
+++ index.js
@@ -1,49 +1,27 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(Vuex)
+Vue.use(VueRouter)
 
-export default new Vuex.Store({
-  state: {
-    count: 0
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
   },
-  mutations: {
-    add(state) {
-      state.count++
-    }
-  },
-  //只有mutations中定义的函数，才有权利修改 state 中的数据
-  mutations: {
-    addN(state, step) {
-      state.count += step
-    },
-    sub(state) {
-      state.count--
-    },
-    subN(state, step) {
-      state.count -= step
-    },
-    
-  },
-  actions: {
-    addAsync(context) {
-      setTimeout(() => {
-        // 在actions中，不能直接修改 state中的数据；
-        // 必须通过 context.commit()触发某个 mutation 才行
-        context.commit('add')
-      }, 1000)
-    },
-    subAsync(context) {
-      setTimeout(() => {
-        // 在actions中，不能直接修改 state中的数据；
-        // 必须通过 context.commit()触发某个 mutation 才行
-        context.commit('sub')
-      }, 1000)
-    }
-  },
-  getters: {
-    showNum(state) => {
-  return '当前最新的数量是[' + state.count + ']'
-}
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+export default router
