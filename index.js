2ae16cada81b97a4ed889a69ac0d834cb92e27bd
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-07 18:26:59 +0800

    vue

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+export function param2Obj(url) {
+    const search = url.split('?')[1]
+    if (!search) {
+      return {}
+    }
+    return JSON.parse(
+      '{"' +
+        decodeURIComponent(search)
+          .replace(/"/g, '\\"')
+          .replace(/&/g, '","')
+          .replace(/=/g, '":"')
+          .replace(/\+/g, ' ') +
+        '"}'
+    )
   }
-})
