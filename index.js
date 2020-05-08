32167f97a35c18caf259224f9698f27dc866d256
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-08 21:59:51 +0800

    mock

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-export function param2Obj(url) {
-    const search = url.split('?')[1]
-    if (!search) {
-      return {}
-    }
-    return JSON.parse(
-      '{"' +
-        decodeURIComponent(search)
-          .replace(/"/g, '\\"')
-          .replace(/&/g, '","')
-          .replace(/=/g, '":"')
-          .replace(/\+/g, ' ') +
-        '"}'
-    )
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
