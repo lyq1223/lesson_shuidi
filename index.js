0fe8fc8248359e5ff9161cea1b6e50567e024910
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-08 22:00:23 +0800

    mock

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vuex from 'vuex'
 
-Vue.use(VueRouter)
+Vue.use(Vuex)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+export default new Vuex.Store({
+  state: {
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
   }
-]
-
-const router = new VueRouter({
-  routes
 })
-
-export default router
