8d57c9a670aed5e22a750bd8d9e7d86b25ea1dd9
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-17 21:58:18 +0800

    vue-shop

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
-
-//以后的项目可以分模块建，mock的模块化
-import Mock from '@/mock/article.js'  //引入mock 开发流程
 
 Vue.config.productionTip = false
 
