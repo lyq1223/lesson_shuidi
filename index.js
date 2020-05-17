568af31b6d49c2ec9f89f33e852b605c3983afaf
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-17 21:55:06 +0800

    vue-shop

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(Vuex)
+Vue.use(VueRouter)
 
-export default new Vuex.Store({
-  state: {
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+export default router
