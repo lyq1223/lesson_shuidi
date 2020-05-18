52ac8fa8f826c6c6498edc6d376c355ffea77691
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-18 12:27:15 +0800

    vue-ordervue-order

--- App.vue
+++ App.vue
@@ -1,34 +1,160 @@
 <template>
   <div id="app">
-    <products />
-    <cart />
-
-    <!-- {{this.$store.state.userInfo}}
-    <div id="nav">    
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/> -->
+    <h1>订单管理</h1>
+    <!-- v-loading数据没来之前先转转 -->
+    <el-table
+    v-loading="listLoading"
+    :data="list"
+    >
+    <el-table-column label="ID"
+    prop="_id"
+    align="center"
+    width="80"
+    >
+    <!-- 用来为表格加些东西,进行格式化
+    slot-scope="{row}代表这一行，拿到行对应的值 -->
+    <template slot-scope="{row}">
+      <span>{{row._id}}</span>
+    </template>
+    </el-table-column>
+    <el-table-column label="Name"
+    prop="name"
+    align="center"
+    width="200"
+    >
+    <template slot-scope="{row}">
+      <span>{{row.name}}</span>
+    </template>
+    </el-table-column>
+    <el-table-column label="OrderDate"
+    prop="orderDate"
+    align="center"
+    width="100"
+    >
+    <template slot-scope="{row}">
+      <span>{{row.orderDate}}</span>
+    </template>
+    </el-table-column>
+    <el-table-column label="状态"
+    prop="status"
+    align="center"
+    width="400"
+    >
+    <template slot-scope="{row}">
+      <span>{{row.status}}</span>
+    </template>
+    </el-table-column>
+    <el-table-column label="邮费"
+    prop="shippingFee"
+    align="center"
+    width="100"
+    >
+    <template slot-scope="{row}">
+      <span>{{row.shippingFee}}</span>
+    </template>
+    </el-table-column>
+    <el-table-column label="单价"
+    prop="total"
+    align="center"
+    width="100"
+    >
+    <template slot-scope="{row}">
+      <span>{{row.total}}</span>
+    </template>
+    </el-table-column>
+    </el-table>
+    <!-- 分页组件:total=""总共的数量有多少 修饰分页器layout 
+    :current-page.sync="page" 同步页-->
+    <el-pagination
+      :current-page.sync="page"
+      :total="total"
+      :pageSize="limit"
+      layout="total, prev, pager, next"
+      @current-change="handleCurrentChange">
+    </el-pagination>
   </div>
 </template>
 <script>
-import ShoppingCart from '@/components/ShoppingCart'
-import ProductList from '@/components/ProductList'
+import Axios from 'axios';
 export default {
-  mounted(){
-    // 单一状态树
-    // console.log(this.$store.state);
-    // console.log(this.$store.state.products.all);
-    // console.log(this.$store.state.cart);
-    // console.log(this.$store.state.cart.items);
-
-    // 获取商品，MVVM，商品列表的展示
-    // 触发动作，去getProduct
-    // this.dispatch('') 
+  data() {
+    return {
+      page:1, //当前页
+      total:100, //一共多少条数据
+      limit:20, //每页显示20条
+      listLoading:true, //加载数据中
+      list: [
+        // {
+        //   "_id": "121212",
+        //   "name": "韭黄",
+        //   "orderDate": new Date(),
+        //   "status": "completed",
+        //   shippingFee: "0.5",
+        //   total: "111"
+        // },
+        // {
+        //   "_id": "121212",
+        //   "name": "韭黄",
+        //   "orderDate": new Date(),
+        //   "status": "canceled",
+        //   shippingFee: "0.6",
+        //   total: "111"
+        // },
+        // {
+        //   "_id": "121212",
+        //   "name": "韭黄",
+        //   "orderDate": new Date(),
+        //   "status": "created",
+        //   shippingFee: "0.7",
+        //   total: "111"
+        // }
+      ]
+    }
   },
-  components: {
-    'products': ProductList, //'products'指定在当前组件中的名字
-    'cart': ShoppingCart
+  mounted(){
+    // setTimeout(()=>{
+    //   this.listLoading = false
+    // },1000)
+    // Axios.post('/api/orders',{
+    //   params: {
+    //     // 分页参数        
+    //   }
+    // })
+    //不能接受动态路由吗？ /api/orders/:page/:limit
+    // Axios.get('/api/orders',{
+    //   params: {
+    //     // 分页参数 
+    //     page: this.page,
+    //     limit: this.limit       
+    //   }
+    // })
+    // Axios.post('/api/orders',{
+      Axios.get('/api/orders',{
+      params: {
+        // 分页参数 
+        page: this.page,
+        limit: this.limit       
+      }
+    })
+    .then(res => {
+      console.log(res);
+      // this.list = res.data.orders
+      this.list = res.data.result
+      setTimeout(()=>{
+        this.listLoading = false
+      },1000)
+    })
+  },
+  methods: {
+    handleCurrentChange(page) {
+      // 翻页响应联动事件
+      console.log(page);
+    }
   }
 }
 </script>
@@ -53,8 +179,7 @@ export default {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
-
-[disabled]{
-  cursor: not-allowed;
-}
+/* .red{
+  color: red;
+} */
 </style>
