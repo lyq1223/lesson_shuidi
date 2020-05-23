23d24b908b55867cae7f04ac1d0d3f8b93f4a7dc
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-23 21:47:55 +0800

    leetcode22

--- 1.js
+++ 1.js
@@ -1,17 +1,19 @@
-var myAtoi = function(str) {
-   
-    // parseInt(string, radix)   将一个字符串 string 转换为 radix 进制的整数， radix 为介于2-36之间的数。
-    const result = parseInt(str, 10)
-    //如果是字符串开头，parseInt会返回NaN 所以要先判断是否是NaN,是就返回0
-    if(isNaN(result)) {
-        return 0;
-    } else if(result < Math.pow(-2,31)) {  //Math.pow可以求几次方:-2^31
-        return Math.pow(-2,31);
-    } else if(result > Math.pow(2, 31)-1) {
-        return Math.pow(2, 31)-1;
-    }
-    return result;
+// 添加右括号时，左括号数量必须大于右括号数量，并且右括号 < n
+var generateParenthesis = function(n) {
+    let res = [];
+    const parent = function(cur, left, right) {
+        if(cur.length === 2*n) {
+            res.push(cur);
+            return;
+        }
+        if(left < n) {
+            parent(cur + '(',left+1, right);
+        }
+        if(right < left){
+            parent(cur+')',left, right+1);
+        }
+    };
+    parent('',0,0);
+    return res;
 };
-console.log(myAtoi("4193 with words"));
-console.log(myAtoi("-91283472332"));
-console.log(myAtoi("words and 987"));
+console.log(generateParenthesis(3))
