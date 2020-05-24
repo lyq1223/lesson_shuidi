115b0e73d906aec971d8e0fcc6343685f6ff6774
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-24 22:04:50 +0800

    leetcode45go

--- 2.go
+++ 2.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func jump(nums []int) int {
+	pos := len(nums) - 1
+	step := 0
+	for pos > 0 {
+		for i :=0; i < pos; i++ {
+			if i + nums[i] >= pos {
+				pos = i
+				step++
+				break
+			}
+		}
+	}
+	return step
+}
+
+func main(){
+	fmt.Println(jump([]int{2,3,1,1,4})) //数组
 }
