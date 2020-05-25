390c44c1652b52a13418584c8f7afe2c73a4e8ae
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-25 18:22:34 +0800

    leetcode53

--- 1.js
+++ 1.js
@@ -1,24 +1,42 @@
-var permute = function(nums) {
-    // let res = [];
-    const res = [];
-    backtrack(res, [], nums);
-    return res;
+var maxSubArray = function(nums) {
+    // 解法1：
+    // let res = [], list = [];
+    // let sum=0;
+    // for(let i = 0; i < nums.length; i++) { 
+    //     // sum += nums[i];
+    //     // console.log(sum, '====');
+    //     if(sum < 0) {
+    //         list.splice(0, i + 1)
+    //         sum = 0;
+    //     }
+    //     list.push(nums[i]);
+    //     sum += nums[i];
+    //     // console.log(sum, '==sum')
+    //     // console.log(list);  
+    //     res.push(sum);       
+    // }
+    // var max = Math.max.apply(null, res);
+    // return max;
 
+    // 解法2，动态规划
+    // 前一个元素大于0（计算的和），将其加到当前元素上
+    let pre = 0, maxAns = nums[0];
+    nums.forEach(element => {
+        pre = Math.max(pre + element, element);
+        maxAns = Math.max(maxAns, pre);
+    });
+    return maxAns;
 };
-//回溯
-function backtrack(res, temList, nums) {
-    //退出条件
-    if (temList.length === nums.length) return res.push([...temList]);
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
 
-    //判断回溯
-    for(let i = 0; i < nums.length; i++) {
-        //因为这个给定的数组是个不重复的，所以只需要判断是否存在某个nums中的值即可
-        if(temList.includes(nums[i])) continue;
-        temList.push(nums[i]);
-        //每次插进去一个，就能再回溯一次，看是不是长度已经满足了一个结果数组
-        backtrack(res, temList, nums);
-        // 能回溯，然后return出来，说明已经成功在结果数组中将上一轮的temList加进去了，那就把temList清空
-        temList.pop();
-    }
-}
-console.log(permute([1,2,3]));
+// let ans = nums[0];
+    // let sum = 0;
+    // for(const num of nums) {
+    //     if(sum > 0) {
+    //         sum += num;
+    //     } else {
+    //         sum = num;
+    //     }
+    //     ans = Math.max(ans, sum);
+    // }
+    // return ans;
