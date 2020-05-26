3c0cfe840fdb51cbfd0c8234f9c0b06906391491
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-26 20:39:31 +0800

    leetcode169

--- 1.js
+++ 1.js
@@ -1,53 +1,28 @@
-/**
- * initialize your data structure here.
- */
-var MinStack = function() {
-    this.list = [];
-    this.minList = [];
-    this.length = 0;
-};
+// 大小为n的数组吗，其中的多数元素，重复的数据出现次数大于n/2 majority
+// [3,2,3]  3 也就是至少一半数据是这个majority,可以假设数组不为空，总是有多数元素
+// [1,2,3]不会存在这种情况，并且给定的数组总是存在多数元素即也不有[1,2,2,3,3,4]
 
-/** 
- * @param {number} x
- * @return {void}
- */
-MinStack.prototype.push = function(x) {
-    this.list[this.length] = x;
-    let min = this.length > 0 ? this.minList[this.length-1] : Infinity;
-    //取出minList中最小的，方便比较x要不要入栈
-    this.minList[this.length]  = min>x ? x :min;
-    this.length++;
-};
-
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
-    this.list.length = --this.length;//??
-    this.minList.length = this.length;//??
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
-        return this.list[this.length - 1];
-
-};
-
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
-        return this.minList[this.length - 1];
-
-};
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(x)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
+// for 用一个字典json，再加一个最大值max 空间复杂度O(n),时间复杂度也是
+//选举算法
+var majorityElement = function(nums){
+    let count = 1, //计数
+    majority = nums[0];//不知道哪个是majority那就先假设一个
+    //算法回归运算，
+    for(let i=1; i<nums.length; i++){
+        //如果再出现这个数，count++
+        //不一样的数，count--
+        //如果count都为0了，那就不是这个数，就让majority = 那个数
+        // 让不一样的数被消灭，然后选举出来的n/2
+        // 从这里投票，相等就加一
+        if(count == 0){
+            majority = nums[i];
+        }
+        if(nums[i] === majority){
+            count++;
+        }else{
+            count--;
+        }        
+    }
+    return majority;
+}
+console.log(majorityElement([1,1,1,2,2,2,2,3])); 
