746dbcc1aea4458786154e8899500c7b16907d4a
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-27 22:14:48 +0800

    leetcode199 2

--- 2.js
+++ 2.js
@@ -1,23 +1,50 @@
-// 栈
-var MinStack = function() {
-    this.stack = [];
-    this.min_stack = [Infinity];
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
 }
+const n1 = new TreeNode(1);
+const n2 = new TreeNode(2);
+const n3 = new TreeNode(3);
+// const n4 = new TreeNode(null);
+const n5 = new TreeNode(4);
+n1.left = n2;
+n1.right = n3;
+// n2.left = n4;
+n2.right = n5;
 
-MinStack.prototype.push = function(x) {
-    this.stack.push();
-    this.min_stack.push(Math.min(this.min_stack[this.min_stack.length - 1], x)); //!!
+// bfs
+// var rightSideView = function(root) {
+//     let res = [];
+//     let que = [root];
+//     while(que.length) {
+//         let size = que.length;
+//         while(size) {
+//             let lastnode = que.shift();
+//             if(size === 1) res.push(lastnode.val);
+//             if(lastnode.left) que.push(lastnode.left);
+//             if(lastnode.right) que.push(lastnode.right);
+//             size--; //这个时候，就不能把减减放在while，要在判断完if之后再减
+//         }
+//     }
+//     return res;
+// }
+// console.log(rightSideView(n1));
+
+// dfs
+var rightSideView = function(root) {
+    if(!root) return []
+    let arr = []
+    dfs(root, 0, arr)
+    return arr
+};
+function dfs(root, step, res) {
+    if(root) {
+        // 当数组长度等于当前 深度 时, 把当前的值加入数组
+        if(res.length === step) {
+            res.push(root.val);
+        }
+        // console.log(step, '-------', res)
+        dfs(root.right, step + 1, res) // 先从右边开始, 当右边没了, 再轮到左边
+        dfs(root.left, step + 1, res)
+    }
 }
-MinStack.prototype.pop = function() {
-    this.x_stack.pop();
-    this.min_stack.pop();
-};
-
-MinStack.prototype.top = function() {
-    return this.x_stack[this.x_stack.length - 1];
-};
-
-MinStack.prototype.getMin = function() {
-    return this.min_stack[this.min_stack.length - 1];
-};
-
