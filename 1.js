dc9680507a9899f588a661b907fc9bf55b0b394b
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-27 22:14:02 +0800

    leetcode199

--- 1.js
+++ 1.js
@@ -1,28 +1,65 @@
-// 大小为n的数组吗，其中的多数元素，重复的数据出现次数大于n/2 majority
-// [3,2,3]  3 也就是至少一半数据是这个majority,可以假设数组不为空，总是有多数元素
-// [1,2,3]不会存在这种情况，并且给定的数组总是存在多数元素即也不有[1,2,2,3,3,4]
-
-// for 用一个字典json，再加一个最大值max 空间复杂度O(n),时间复杂度也是
-//选举算法
-var majorityElement = function(nums){
-    let count = 1, //计数
-    majority = nums[0];//不知道哪个是majority那就先假设一个
-    //算法回归运算，
-    for(let i=1; i<nums.length; i++){
-        //如果再出现这个数，count++
-        //不一样的数，count--
-        //如果count都为0了，那就不是这个数，就让majority = 那个数
-        // 让不一样的数被消灭，然后选举出来的n/2
-        // 从这里投票，相等就加一
-        if(count == 0){
-            majority = nums[i];
-        }
-        if(nums[i] === majority){
-            count++;
-        }else{
-            count--;
-        }        
-    }
-    return majority;
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
 }
-console.log(majorityElement([1,1,1,2,2,2,2,3])); 
+const n1 = new TreeNode(1);
+const n2 = new TreeNode(2);
+const n3 = new TreeNode(3);
+// const n4 = new TreeNode(null);
+const n5 = new TreeNode(4);
+n1.left = n2;
+n1.right = n3;
+// n2.left = n4;
+n2.right = n5;
+
+var rightSideView = function(root) {
+    // 如果是个空树
+    if(!root) {
+        return []
+    }
+    let res = []; //放结果
+    let queue = [];//放临时遍历的结点
+    // 从根节点开始，先把root放进queue
+    queue.push(root);
+    //只要还有结点就一直遍历
+    // let lastNode = new TreeNode();
+
+    while(queue.length) {
+        // 将该层的第一个结点放进res中
+        res.push(queue[0].val);
+        //这一层有几个结点
+        let size = queue.length;
+        while(size--) {
+            //遍历到的当前节点，将它的孩子节点放入队列中（下一层），并将该节点出队
+            let lastNode =  queue.shift();
+            if(lastNode.right) queue.push(lastNode.right);
+            if(lastNode.left) queue.push(lastNode.left);
+        }
+    }
+    return res;
+};
+console.log(rightSideView(n1));
+// console.log(n1);
+
+// 如果找左节点
+var leftSideView = function(root) {
+    if(!root) return [];
+    //记录结果的数组
+    let res = [];
+    //记录每一层结点的数组
+    let queue = [];
+    queue.push(root);
+    while(queue.length){
+        res.push(queue[0].val);
+        //用来遍历一层节点
+        let size = queue.length;
+        while(size--) {
+            // 出第一个，放孩子
+            let lastNode = queue.shift();
+            if(lastNode.left) queue.push(lastNode.left);
+            if(lastNode.right) queue.push(lastNode.right);
+        }
+    }
+    return res;
+}
+console.log(leftSideView(n1));
