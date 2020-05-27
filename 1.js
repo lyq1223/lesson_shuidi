3a14da023f5943ccb136bf4f146ea903398efcfa
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-27 22:16:24 +0800

    leetcode222

--- 1.js
+++ 1.js
@@ -1,65 +1,52 @@
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
-const n1 = new TreeNode(1);
-const n2 = new TreeNode(2);
-const n3 = new TreeNode(3);
-// const n4 = new TreeNode(null);
-const n5 = new TreeNode(4);
-n1.left = n2;
-n1.right = n3;
-// n2.left = n4;
-n2.right = n5;
-
-var rightSideView = function(root) {
-    // 如果是个空树
-    if(!root) {
-        return []
+var maximalSquare = function(matrix) {
+    let row = matrix.length, col = matrix[0].length;
+    if(row == 0 || col == 0) {
+        return 0;
     }
-    let res = []; //放结果
-    let queue = [];//放临时遍历的结点
-    // 从根节点开始，先把root放进queue
-    queue.push(root);
-    //只要还有结点就一直遍历
-    // let lastNode = new TreeNode();
-
-    while(queue.length) {
-        // 将该层的第一个结点放进res中
-        res.push(queue[0].val);
-        //这一层有几个结点
-        let size = queue.length;
-        while(size--) {
-            //遍历到的当前节点，将它的孩子节点放入队列中（下一层），并将该节点出队
-            let lastNode =  queue.shift();
-            if(lastNode.right) queue.push(lastNode.right);
-            if(lastNode.left) queue.push(lastNode.left);
+    let maxlen = 0;
+    for(let i = 0; i < row; i++) {
+        for(let j = 0; j < col; j++) {
+            if(matrix[i][j] == "1") {
+                maxlen = max(maxlen, 1);
+                // 左上角为1，就开始在左，下两边找还有没有为1的，因为是正方形，所以要取宽高中最短的那边
+                curmax = min(row - i, col - j);
+                // 开始每次以一行，一列开始找
+                for(let k = 1; k < curmax; k++) {
+                    let flag = true;
+                    // 先找对角线，之后好找范围
+                    if(matrix[i + k][j + k] == "0") {
+                        break;
+                    }
+                    // 找对角线里面的
+                    for(let m = 0; m < k; m++) {
+                        if(matrix[i + k][j + m] == "0" || matrix[i + m][j + k] == "0") {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag) {
+                        maxlen = Math.max(maxlen, k+1);
+                    } else {
+                        break;
+                    }
+                }
+            }
         }
     }
-    return res;
+    return maxlen * maxlen;
 };
-console.log(rightSideView(n1));
-// console.log(n1);
-
-// 如果找左节点
-var leftSideView = function(root) {
-    if(!root) return [];
-    //记录结果的数组
-    let res = [];
-    //记录每一层结点的数组
-    let queue = [];
-    queue.push(root);
-    while(queue.length){
-        res.push(queue[0].val);
-        //用来遍历一层节点
-        let size = queue.length;
-        while(size--) {
-            // 出第一个，放孩子
-            let lastNode = queue.shift();
-            if(lastNode.left) queue.push(lastNode.left);
-            if(lastNode.right) queue.push(lastNode.right);
-        }
+function max(x,y) {
+    if(x > y) {
+        return x;
+    } else {
+        return y;
     }
-    return res;
 }
-console.log(leftSideView(n1));
+function min(x,y) {
+    if(x < y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+console.log(maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]));
