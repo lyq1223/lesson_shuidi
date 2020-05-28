40e6637fda12cd4cbc14c8b612c85c9ea5fa1cb7
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-28 20:11:03 +0800

    leetcode445

--- 1.js
+++ 1.js
@@ -1,29 +1,84 @@
-
-var lengthOfLIS = function(nums){
-    var lengthL=[1]; //最大序列长度
-    if(nums.length==0)
-        return 0;    
-    for(var i=1;i<nums.length;i++){ //ii用来计算序列长度
-        var res=1;
-        for(var ii=0;ii<i;ii++){
-            if(nums[ii]<nums[i]){
-                res=Math.max(res,lengthL[ii]+1);
-                console.log(lengthL[ii]+'=====')
-            }
-        }
-        lengthL[i]=res;
-    }
-    console.log(lengthL);
-    return Math.max(... lengthL);//lengthL中的最大值
+function listNode(val) {
+    this.val = val;
+    this.next = null;
 }
-// console.log(lengthOfLIS([10,9,2,5,3,7,101,18]));
-console.log(lengthOfLIS([4,10,4,3,8,9]));
+const n1 = new listNode(7);
+const n2 = new listNode(2);
+const n3 = new listNode(4);
+const n4 = new listNode(3);
+n1.next = n2;
+n2.next = n3;
+n3.next = n4;
 
-// if(nums[i+1] > nums[i]){
-        //     lengthL.push(nums[i+1]);
-        // } else {
-            
-        //     count = Math.max(count,lengthL.length+1);
+const a1 = new listNode(5);
+const a2 = new listNode(6);
+const a3 = new listNode(4);
+a1.next = a2;
+a2.next = a3;
+// var reverseList = function(head) {
+//     var list = head;
+//     var p = list;
+//     var q = null;
+   
+//    if(p == null)	//做个判断啊！！非常傻的一个错呜呜呜
+//        return null;
 
-        //     lengthL.splice(0,lengthL.length);
-        // }
+//    while(p.next !== null) {
+//        q = p.next;
+//        p.next = q.next;
+//        q.next = list;
+//        list = q;     
+//    }
+//    return list;
+// };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+var addTwoNumbers = function(l1, l2) {
+    let link1 = reverse(l1);
+    let link2 = reverse(l2);
+    // let p = link1;
+    // let q = link2;
+    // 进一标志
+    let mark = 0;
+    let res = new listNode();
+    let cur = res;
+    while (link1 || link2 || mark === 1) { //一定要加mark === 1
+        let val = (link1 ? link1.val : 0) + (link2 ? link2.val : 0) + mark;
+        //按位或赋值运算符使用两个操作值的二进制表示，执行按位或运算，并把结果赋给变量
+        mark = val / 10 | 0;
+        cur.next = new listNode(val % 10);
+        cur = cur.next;
+        if (link1) link1 = link1.next;
+        if (link2) link2 = link2.next;
+    }
+    // 将结果再次翻转
+    return reverse(res.next);
+
+     function reverse(l) {
+        // 迭代
+        let cur = l;
+        let pre = null;
+        while (cur) {
+            let temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+     }
+};
+
+console.log(addTwoNumbers(n1,a1));
+// console.log(a1);
