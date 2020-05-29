6d96f8c7960edaa200878c99ce2d3537de22e84d
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-29 19:27:10 +0800

    leetcode887

--- 1.js
+++ 1.js
@@ -1,40 +1,16 @@
-function listNode(val) {
-    this.val = val;
-    this.next = null;
-}
-const n1 = new listNode(1);
-const n2 = new listNode(2);
-const n3 = new listNode(3);
-const n4 = new listNode(4);
-const n5 = new listNode(5);
-// const n6 = new listNode(6);
-n1.next = n2;
-n2.next = n3;
-n3.next = n4;
-n4.next = n5;
-// n5.next = n6;
-// n6.next = null;
-
-var middleNode = function(head) {
-    let fast = head, slow = head;
-    while(fast != null && fast.next != null) {
-        fast = fast.next.next;
-        slow = slow.next;
+var superEggDrop = function(K, N) {
+    let dp = Array(K + 1).fill(0).map(()=>new Array(N + 1).fill(0));
+    // console.log(dp);
+    //i是有i个鸡蛋，j是j次扔鸡蛋的测得的最多楼层
+    //用i,j反求N
+    for(let j = 1; j <N; j++){
+        for(let i = 1; i< K; i++){
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j] + 1;
+            if(dp[i][j] >= N) {
+                return j
+            }
+        }
     }
-    return slow.val;
+    return N;
 };
-// console.log(n1);
-console.log(middleNode(n1));
-
-//java
-//  快慢指针，慢指针始终比快指针慢1/2，快指针到终点，慢指针到中间。
-// class Solution {
-//     public ListNode middleNode(ListNode head) {
-//         ListNode f = head, s = head;
-//         while(f != null && f.next != null) {
-//             f = f.next.next;
-//             s = s.next;
-//         }
-//         return s;
-//     }
-// }
+console.log(superEggDrop(5,6))
