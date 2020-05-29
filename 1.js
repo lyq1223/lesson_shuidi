abcd93f3cba5537f9604adfd4bb86feb17b2e451
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-29 19:18:37 +0800

    leetcode876

--- 1.js
+++ 1.js
@@ -1,13 +1,40 @@
-var minimumLengthEncoding = function(words) {
-    let set = new Set(words);
-    for(let item of set) {
-        for(let i=1; i< item.length;i++){
-            let last = item.slice(i);
-            set.has(last) && set.delete(last);
-        }
+function listNode(val) {
+    this.val = val;
+    this.next = null;
+}
+const n1 = new listNode(1);
+const n2 = new listNode(2);
+const n3 = new listNode(3);
+const n4 = new listNode(4);
+const n5 = new listNode(5);
+// const n6 = new listNode(6);
+n1.next = n2;
+n2.next = n3;
+n3.next = n4;
+n4.next = n5;
+// n5.next = n6;
+// n6.next = null;
+
+var middleNode = function(head) {
+    let fast = head, slow = head;
+    while(fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
     }
-    let count = 0;
-    set.forEach(item => count += item.length + 1);
-    return count
+    return slow.val;
 };
-console.log(minimumLengthEncoding(["time", "me", "bell"]));
+// console.log(n1);
+console.log(middleNode(n1));
+
+//java
+//  快慢指针，慢指针始终比快指针慢1/2，快指针到终点，慢指针到中间。
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode f = head, s = head;
+//         while(f != null && f.next != null) {
+//             f = f.next.next;
+//             s = s.next;
+//         }
+//         return s;
+//     }
+// }
