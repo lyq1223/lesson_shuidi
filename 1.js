826b00009a98b8219503d3c67e8aefe786a152a0
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-29 19:12:53 +0800

    leetcode820

--- 1.js
+++ 1.js
@@ -1,23 +1,13 @@
-var getMaxRepetitions = function(s1, n1, s2, n2) {
-    let arr1 = s1.split("");
-    let arr2 = s2.split("");
-    let index = 0;
-    let res = 0;
-    if(arr1.length < arr2.length) {
-        return 0;
-    }
-    //有n1个s1,所以循环n1次，就能匹配到相应长度的s1
-    for(let j = 0; j < n1; j++){
-        for(let i = 0; i < arr1.length; i++){
-            if(arr1[i] === arr2[index]) { //依次匹配arr2中的字符，
-                index++
-            }
-            if(index === arr2.length) {
-                index = 0;
-                res++;
-            }
+var minimumLengthEncoding = function(words) {
+    let set = new Set(words);
+    for(let item of set) {
+        for(let i=1; i< item.length;i++){
+            let last = item.slice(i);
+            set.has(last) && set.delete(last);
         }
     }
-    return parseInt(res/n2);
+    let count = 0;
+    set.forEach(item => count += item.length + 1);
+    return count
 };
-console.log(getMaxRepetitions("acb",4,"ab",2));
+console.log(minimumLengthEncoding(["time", "me", "bell"]));
