4177889b2c2a2f619d3302e460027ac458e9b4e0
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-30 18:10:56 +0800

    leetcode892go

--- 2.go
+++ 2.go
@@ -1,20 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func maxSubArray(nums []int) int {
-	max := nums[0]
-	for i := 1; i < len(nums); i++ {
-		if nums[i] + nums[i - 1] > nums[i] {
-			nums[i] += nums[i - 1]
-		} 
-		if nums[i] > max {
-			max = nums[i]
+func surfaceArea(grid [][]int) int {
+
+	surfaceAreaCount := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid); j++ {
+			count := grid[i][j]
+
+			if count == 0 {
+				continue
+			}
+			surfaceAreaCount += 2 + 4*count
+
+			if i-1 >= 0 {
+				surfaceAreaCount -= int(math.Min(float64(count), float64(grid[i-1][j])))
+			}
+			if i+1 < len(grid) {
+				surfaceAreaCount -= int(math.Min(float64(count), float64(grid[i+1][j])))
+			}
+			if j-1 >= 0 {
+				surfaceAreaCount -= int(math.Min(float64(count), float64(grid[i][j-1])))
+			}
+			if j+1 < len(grid) {
+				surfaceAreaCount -= int(math.Min(float64(count), float64(grid[i][j+1])))
+			}
 		}
 	}
-	return max
+	return surfaceAreaCount
 }
 
-func main(){
-	fmt.println(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+func main() {
+	fmt.Println(surfaceArea([][]int{{1, 2}, {3, 4}})) //[[1,2],[3,4]]
 }
