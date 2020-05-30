e6d7a53dde032e5862d701c8977d08a50c618c41
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-30 18:16:00 +0800

    leetcode914

--- 1.js
+++ 1.js
@@ -1,13 +1,38 @@
-var sortArray = function(nums) {
-    for(let i=0; i<nums.length -1; i++) {
-        for(let j = 0; j < nums.length -1 -i; j++) {
-            if(nums[j] > nums[j+1]) {
-                var tmp = nums[j];
-                nums[j] = nums[j+1];
-                nums[j+1] = tmp;
-            }
-        }
-    }
-    return nums
-};
-console.log(sortArray([5,1,1,2,0,0]));
+
+var hasGroupsSizeX = function(deck) {
+    
+//解法一
+//     let map = new Map();
+//     deck.forEach(element => {
+//         map.set(element,map.has(element) ? map.get(element)+1 : 1)
+//     });
+//     let mapArr = [...map.values()];
+//     let max = 0;
+//     for(let i=0; i< mapArr.length; i++){
+//         max = Math.max(max,mapArr[i])
+//     }
+//     let x = 2;
+//     while (x <= max) {
+//         if(mapArr.every(ele => ele%x == 0)) return true
+//         x++;
+//     }
+//     return false
+
+//     //用最大公约数 辗转相除法
+// };
+// // console.log(hasGroupsSizeX([1,1,2,2,2,2]));
+// console.log(hasGroupsSizeX([1,1,1,2,2,3,3]));
+
+//解法二 最大公约数，辗转相除法
+
+
+//思路一错错错
+// let count = deck.length;
+// let set = new Set(deck);
+// // set.add(deck);
+// let size = set.size;
+// if (count%size === 0){
+//     return true
+// } else {
+//     return false
+// }
