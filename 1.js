80489346b91998e038e0c97a13279eea18ba7e92
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-30 18:09:58 +0800

    leetcode892

--- 1.js
+++ 1.js
@@ -1,16 +1,42 @@
-var superEggDrop = function(K, N) {
-    let dp = Array(K + 1).fill(0).map(()=>new Array(N + 1).fill(0));
-    // console.log(dp);
-    //i是有i个鸡蛋，j是j次扔鸡蛋的测得的最多楼层
-    //用i,j反求N
-    for(let j = 1; j <N; j++){
-        for(let i = 1; i< K; i++){
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j] + 1;
-            if(dp[i][j] >= N) {
-                return j
+var surfaceArea = function(grid) {
+
+    //初始化表面积
+    var surfaceAreaCount = 0;
+
+    for(let i = 0; i < grid.length; i++){
+        for(let j=0; j < grid.length; j++)
+        {
+            //该位置立方体的个数
+            var count = grid[i][j];
+
+            //该位置上0个立方体
+            if(!count) {
+                continue;
             }
-        }
+            //有立方体
+            if(count >= 0) {
+                surfaceAreaCount += 2 + 4 * count;
+            }
+
+            //减去重叠部分
+            //左边有
+            if(i-1 >= 0) {
+                surfaceAreaCount -= Math.min(count, grid[i-1][j]);
+            }
+            //右边有 注意是小于横坐标的最右边
+            if(i+1 < grid.length) {
+                surfaceAreaCount -= Math.min(count, grid[i+1][j]);
+            }
+            //上边有
+            if(j-1 >= 0) {
+                surfaceAreaCount -= Math.min(count, grid[i][j-1]);
+            }
+            //下边有，注意是小于纵坐标的最下面
+            if(j+1 < grid.length) {
+                surfaceAreaCount -= Math.min(count, grid[i][j+1]);
+            }
+        } 
     }
-    return N;
-};
-console.log(superEggDrop(5,6))
+    return surfaceAreaCount
+}
+console.log(surfaceArea([[1,2],[3,4]]));
