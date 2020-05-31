0c1c033c568b45436369303fc1f84523149a160a
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-05-31 18:16:54 +0800

    leetcode1111

--- 1.js
+++ 1.js
@@ -1,39 +1,22 @@
-var numRookCaptures = function(board) {
-    let count = 0;
-    let x , y ;
-    
-    //记录车的位置
-    for(let i=0;i < 8; i++) {
-        for(let j = 0;j<8;j++){
-            if(board[i][j] === 'R') {
-                x = i;
-                y = j;               
-            }
-        }
-    }
-    //车的行动方向 西->东(x+1,0) 东->西(x-1,0) 南->北(0,y+1) 北->南(0,y-1)
-    const rx = [1,0,-1,0];
-    const ry = [0,-1,0,1];
-    //按4个方向走
-    for(let i=0;i<4;i++){
-        //死循环，按一个方向走到底
-        for(let j=0; ;j++){
-            //记录车在x轴的移动后的坐标
-            const mx = x + j*rx[i];
-            //y轴
-            const my = y + j*ry[i];
-             
-            //限制车移动的范围,在棋盘范围内，且不碰到象
-            if(mx < 0 || mx >=8 || my < 0 || my >=8 || board[mx][my] === 'B') {
-                break;
-            }
-            if(board[mx][my] === 'p') {
-                count++;
-                break;
-            }
+var maxDepthAfterSplit = function(seq) {
+    let dep = 0;
+    //map() 方法返回一个新数组，不会改变原始数组。同时新数组中的元素为原始数组元素调用函数处理后的值，并按照原始数组元素顺序依次处理元素。
 
+    // 注意：map() 不会对空数组进行检测。所以要先处理。
+    //item 当前元素的值（必选），index当前索引值（可选）
+    //碰到"("深度加一，(深度为奇数就记为1，为偶数就记为0);碰到,")"先记录下深度%2，再将深度减一return 先前记录下来的dep%2
+    //为(深度加一，
+    return seq.split("").map((item,index)=>{
+        if(item === '(') { 
+            ++dep;
+            return dep % 2;
+        } else {  //碰到")"
+            let tmp = dep % 2; //)的深度 肯定跟前面与之对应的(的相同，放入数组中，是为一还是为零而和前面的(的 一致，都为dep % 2
+            --dep;  //降深度
+            return tmp;
         }
-    }
-     return count;
+    });
+    // return seq;
 };
-console.log(numRookCaptures([[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".","R",".",".",".","p"],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],[".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."]]));
+console.log(maxDepthAfterSplit("()(())()"));
+console.log(maxDepthAfterSplit("(()())"));
