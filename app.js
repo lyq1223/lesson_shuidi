01bae68a54536bbe8ebc5fa3a642ba4cd6c63f38
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-06-02 21:04:52 +0800

    void_a

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
-//引入express框架
-const express = require('express');
-//路径处理模块
-const path = require('path');
-const bodyParser = require('body-parser');
-//创建网站服务器
-const app = express();
-
-app.use(bodyParser.json());
-
-//静态资源访问服务功能
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/first', (req, res) => {
-    res.send('Hello Ajax');
-});
-
-app.get('/responseData', (req, res) => {
-    res.send({ "name": "zs" })
-});
-
-app.get('/get', (req, res) => {
-    res.send(req.query);
-});
-app.post('/post', (req, res) => {
-    res.send(req.body);
-});
-
-app.post('/json', (req, res) => {
-    res.send(req.body);
-});
-
-
-
-//端口监听
-app.listen(3000);
-//控制台提示输出
-console.log('服务器启动成功')
+var http = require('http');
+http.createServer((req, res) => {
+    if(req.url == '/new_page_not_go') {
+        res.writeHead(204); //2开头的表示成功，204成功了，不过没什么可以返回的
+        return
+    }
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'})
+    // 连接后就有一个Tcp管道，就能写入
+    // 写入字节码
+    res.write("hello world");
+    res.end(`
+        <html>
+            <head></head>
+            <body>
+                <a href="/new_page_not_go"></a>
+            </body>
+        </html>
+    `)
+})
