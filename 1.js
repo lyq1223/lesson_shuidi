15495e7e73159bb6dd43bf33a7bf3ade4ec45506
lyq1223 <56785439+lyq1223@users.noreply.github.com>
2020-06-05 18:41:48 +0800

    状态机

--- 1.js
+++ 1.js
@@ -1,58 +1,87 @@
-// 1.
-// var minCount = function(coins) {
-//     let res = 0;
-//     for(let i = 0; i<coins.length; i++) {
-//         if(coins[i] % 2 === 0) {
-//             res += coins[i] / 2
-//         }
-//         else {
-//             res += Math.floor(coins[i] / 2) +1
-//             // console.log(res);
-//         }
-//     }
-//     // res +=res;
-//     return res;
-// };
-// // console.log(minCount([2,3,10]));
-// console.log(minCount([4,2,1]));
-
-
-
-// 2.1错
-// var numWays = function(n, relation, k) {
-    
-//     relation.sort((a, b)=> a[0]-b[0]);
-   
-//     let res = [relation[0]];
-    
-//     for(let i = 0; i < relation.length; i++) {
-//      for( let j=0; j<k; j++){
-//         if(res[res.length - 1][1] === relation[i][0] && k >= 0) {
-//             res[res.length-1][1] = relation[i][1];
-//             k--;
-//         }            
-//     }
-//         if(res[res.length-1][1] === n-1 && k === 0) {
-//             res.push(relation[i]);            
-//         }
-//     }
-//     return res;
-// };
-
-
-// 2.
-// var numWays = function(n, relation, k) {
-//     const result = [];
-//     for (let i = 0; i <= k; i++) {
-//         result.push(new Array(n).fill(0));
-//     }
-//     result[0][0] = 1;
-//     for (let i = 1; i <=k; i++) {
-//         for (let j = 0; j < relation.length; j++) {
-//             result[i][relation[j][1]] += result[i - 1][relation[j][0]];
-//         }
-//     }
-//     return result[k][n - 1];
-// };
-// console.log(numWays(3,[[0,2],[2,1]],2));
-// console.log(numWays(5, [[0,2],[2,1],[3,4],[2,3],[1,4],[2,0],[0,4]], 3));
+// a
+function match(string) {
+    for (let c of string) {
+      if (c === 'a') return true;
+    }
+    return false;
+  }
+  // ab
+  function matchAb(string) {
+    let foundA = false;
+    for (let c of string) {
+      if (c === 'a') {
+        foundA = true;
+      } else if (foundA && c === 'b') {
+        return true;
+      } else {
+        // 重新开始 匹配 ab  foundA 失效了
+        foundA = false;
+      }
+    }
+    return false;
+  }
+  console.log(matchAb('acbab'))
+  // abcdef
+  function matchAbcdef(string) {
+    let foundA = false;
+    let foundB = false;
+    let foundC = false;
+    let foundD = false;
+    let foundE = false;
+    for (let c of string) {
+      if (c === 'a') {
+        foundA = true;
+      } else if (foundA && c === 'b') {
+        foundB = true;
+      } else if (foundB && c === 'c') {
+        foundC = true;
+      } else if (foundC && c === 'd') {
+        foundD = true;
+      } else if (foundD && c === 'e') {
+        foundE = true;
+      } else if (foundE && c === 'f') {
+        return true;
+      } else {
+        // 重新开始 匹配 ab  foundA 失效了
+        foundA = false;
+        foundB = false;
+        foundC = false;
+        foundD = false;
+        foundE = false;
+      }
+    }
+    return false;
+  }
+  // abc
+  function newMatch(string) {
+    let state = foundA;
+    for (let c of string) {
+      state = state(c)
+    }
+    return state === end
+  }
+  function foundA(c) {
+    if (c === 'a') {
+      return foundB;
+    } else {
+      return foundA;
+    }
+  }
+  function foundB(c) {
+    if (c === 'b') {
+      return foundC;
+    } else {
+      return foundA;
+    }
+  }
+  function foundC(c) {
+    if (c === 'c') {
+      return end;
+    } else {
+      return foundA;
+    }
+  }
+  function end() {
+    return end
+  }
+  console.log(newMatch('abcbacbc'))
